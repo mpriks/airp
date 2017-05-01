3d24b76fd331ac0235664af1e676287413cc3870
Martin P <mpriks@gmail.com>
2017-05-01 19:57:17 +0300

    tuvastab meetme
    Tuvastab meetme 'taotlusvoorTahis' järgi

---
+++ tuvasta_meede.py
@@ -0,0 +1,31 @@
+import json
+
+
+
+json_string = open('sisend_json.txt', 'r', encoding="utf8").read()
+parsed_json = json.loads(json_string)
+
+toimik = parsed_json.get('toimik')
+taotlusvoor_tahis = toimik.get('taotlusvoorTahis')
+
+print(toimik['taotlusvoorTahis'] + ' ' + toimik['meedeNimi'] + '\n')
+
+
+
+# Leader 19.3
+if taotlusvoor_tahis == '19.3.2016':
+    print('Leader')
+
+
+# Liit 4 voor
+elif taotlusvoor_tahis == '4.1.2017_4':
+    print('Liit')
+
+
+# Väikesed 4 voor
+elif taotlusvoor_tahis == '6.3.2017_4':
+    print('Väikesed')
+
+
+else:
+    print('Tähelepanu! Viga! \nTaotlusvoor tähisega ' + taotlusvoor_tahis + ' tarvis pole skripti arendatud!')
