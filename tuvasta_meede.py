03b0c831d31cfcda8ce35cf5c917ef3daf8320ab
Martin P <mpriks@gmail.com>
2017-05-02 23:11:08 +0300

    lisasin # vaikesed_4voor # liitmeede_liikmetega # leader_19_2 # leader_19_3 det.andmed sammu leidmise funktsiooni

--- tuvasta_meede.py
+++ tuvasta_meede.py
@@ -1,8 +1,11 @@
 import json
 
 
-
-json_string = open('C:\\Users\\martinpr\\Documents\\GitHub\\sisend_failid\\leader_19_2', 'r', encoding="utf8").read()
+# vaikesed_4voor
+# liitmeede_liikmetega
+# leader_19_2
+# leader_19_3
+json_string = open('C:\\Users\\martinpr\\Documents\\GitHub\\sisend_failid\\vaikesed_4voor', 'r', encoding="utf8").read()
 parsed_json = json.loads(json_string)
 
 toimik = parsed_json.get('toimik')
@@ -12,25 +15,63 @@ print(toimik['taotlusvoorTahis'] + ' ' + toimik['meedeNimi'] + '\n')
 
 
 
+def det_andmed_bamKysimuKood(obj_detailandmed):
+    i = 0
+
+    print('\n' + 'Detailandmed sammus küsimusi kokku ' + str(len(obj_detailandmed)) + ':' + '\n')
+
+    for rida in obj_detailandmed:
+
+        print("Andmeväli " + str(i) + ": " + rida['bamKysimusKood'])
+        i = i + 1
+
+        if 'vaartus' in rida:
+            print("Väärtus: " + rida['vaartus'])
+
+        elif 'vaartusNimekiri' in rida:
+            # tagastab list vaartusNimekiri kõik elemendid komaga eraldatult
+            print(', '.join(rida['vaartusNimekiri']))
+
+        elif 'lisadokumendid' in rida:
+            # tagastab listi 'lisadokumendid' element indeksiga 0 ja võtme 'nimetus' väärtuse
+            print("Faili nimi: " + rida['lisadokumendid'][0]['nimetus'])
+
+        print()
+
+
+
+
 # Leader 19.3
 if taotlusvoor_tahis == '19.3.2016':
-    print('Leader 19.3')
+
+    obj_detailandmed = parsed_json.get('sisu').get('leader193Toetustaotlus').get('detailandmed')
+
+    det_andmed_bamKysimuKood(obj_detailandmed)
 
 
 
 # Leader 19.2
 if taotlusvoor_tahis == '19.2.2016':
-    print('Leader 19.2')
+
+    obj_detailandmed = parsed_json.get('sisu').get('leader192Toetustaotlus').get('detailandmed')
+
+    det_andmed_bamKysimuKood(obj_detailandmed)
 
 
 # Liit 4 voor
 elif taotlusvoor_tahis == '4.1.2017_4':
-    print('Liit')
+
+    obj_detailandmed = parsed_json.get('sisu').get('liitmeedeToetustaotlus').get('detailandmed')
+
+    det_andmed_bamKysimuKood(obj_detailandmed)
 
 
 # Väikesed 4 voor
 elif taotlusvoor_tahis == '6.3.2017_4':
-    print('Väikesed 4 voor')
+
+    obj_detailandmed = parsed_json.get('sisu').get('vaikesteToetustaotlus').get('detailandmed')
+
+    det_andmed_bamKysimuKood(obj_detailandmed)
 
 
 else:
